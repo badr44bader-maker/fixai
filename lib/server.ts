30232d531df5a35a723f2de5cce1a7b92c6deba7
badr44bader-maker <badr44bader@gmail.com>
2026-09-24 00:47:37 +0100

    Delete lib directory

--- lib/server.ts
+++
@@ -1,8 +0,0 @@
-import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
-export function createClient() {
-  const c = cookies();
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-    cookies: { getAll: () => c.getAll(), setAll: (l) => { try { l.forEach(({ name, value, options }) => c.set(name, value, options)); } catch {} } },
-  });
-}
