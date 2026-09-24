a1f7cf509d384dade3d52b7bd5a2d025814c5465
badr44bader-maker <badr44bader@gmail.com>
2026-09-24 01:19:19 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ar" dir="rtl">
-      <body style={{margin:0,background:"#000",color:"#fff",fontFamily:"Arial"}}>
-        {children}
-      </body>
-    </html>
-  );
+    <html lang="ar"><body>{children}</body></html>
+  )
 }
