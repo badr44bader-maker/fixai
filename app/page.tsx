74870a7813b215328568784de2b991ba191e8344
badr44bader-maker <badr44bader@gmail.com>
2026-09-24 01:24:20 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <main style={{minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",fontFamily:"Arial"}}>
-      <div style={{fontSize:"64px"}}>🔧</div>
-      <h1 style={{fontSize:"28px",marginTop:"20px"}}>FixAI خدام!</h1>
-      <p style={{color:"#aaa",marginTop:"10px"}}>الموقع ديالك واجد</p>
+    <main style={{minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column"}}>
+      <div style={{fontSize:"64px"}}>FixAI</div>
+      <h1>FixAI خدام!</h1>
+      <p>الموقع واجد</p>
     </main>
   );
 }
